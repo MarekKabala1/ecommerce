b560c837547157e95267dc74e2707f913f8a99a3
[Marek] <marekkabala1@gmail.com>
2023-06-22 07:11:07 +0100

    fixing bug to make dashbord invisible if you are not logged in

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import SideNav from '@/components/SideNav';
 import TopNav from '@/components/TopNav';
-import { useSession } from 'next-auth/react';
+import { useSession, signIn } from 'next-auth/react';
 
 export default function Home() {
 	const { data: session } = useSession();
-	return (
+
+	return session ? (
 		<>
 			<div className='bg-blue-900 flex flex-col h-screen'>
 				<TopNav />
@@ -16,5 +17,15 @@ export default function Home() {
 				</div>
 			</div>
 		</>
+	) : (
+		<div className=' bg-blue-900 flex h-screen w-screen items-center justify-center'>
+			<button
+				onClick={() => {
+					signIn('google');
+				}}
+				className='bg-white rounded-lg p-2 px-4 '>
+				Log in with Google
+			</button>
+		</div>
 	);
 }
